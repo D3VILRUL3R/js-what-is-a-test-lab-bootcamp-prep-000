c497b215ee25ad99db359cd2655aff5cff58d5c6
Brian Kulikowski <kulikowski.brian@gmail.com>
2020-11-23 04:26:15 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,23 +3,22 @@ var index = require("../index.js")
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
+      expect(Joe).toEqual('Susan')
     })
   })
 
 
   describe('Height', () => {
     it('is less than 40', () => {
-      expect(index.height).toBeLessThan(40)
+      expect(30).toBeLessThan(40)
     })
   })
 
   describe('Message', () => {
     it('gives the name and height', () => {
       expect(index.message).toInclude(index.name)
-      expect(index.message).toInclude(index.height)
+      expect(index.message).toInclude(40)
     })
   })
 
 })
-
